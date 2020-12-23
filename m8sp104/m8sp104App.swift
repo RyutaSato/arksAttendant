3bdc25ee62c0a39868250fb2b73fd9f4b4e40563
maron8saton <sato.ryuta.main@gmail.com>
2020-12-23 15:31:38 +0900

    Initial Commit

---
+++ m8sp104/m8sp104App.swift
@@ -0,0 +1,17 @@
+//
+//  m8sp104App.swift
+//  m8sp104
+//
+//  Created by 佐藤竜太 on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct m8sp104App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
